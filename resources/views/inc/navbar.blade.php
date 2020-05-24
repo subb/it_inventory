247f849e22bc2a9a347b565ca535c4cf08961710
sub <subb.suanduang@gmail.com>
2020-05-24 22:59:38 +0700

    edit navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -10,7 +10,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+                <li class="nav-item">
+                <a href="{{route('phones.index')}}" class="nav-link">Phone</a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
